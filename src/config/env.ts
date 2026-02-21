aa8515ac883ca731d1074c017137e6a812df8584
ThiagoSilsa <thiagosantosprog@gmail.com>
2026-02-21 17:01:58 -0300

    feat(env.ts): Isolei a lógica de acesso as variaveis de ambiente em config

---
+++ src/config/env.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+
+// Carrega as variáveis de ambiente do arquivo .env
+dotenv.config();
+
+const env = {
+    // Posta do servidor
+    PORT: process.env.PORT! || 3000,
+
+    // Configurações do banco de dados
+    DB_HOST: process.env.DB_HOST!,
+    DB_PORT: parseInt(process.env.DB_PORT!) || 3306,
+    DB_NAME: process.env.DB_NAME!,
+    DB_USER: process.env.DB_USER!,
+    DB_PASSWORD: process.env.DB_PASSWORD!,
+}
+
+export default env;
