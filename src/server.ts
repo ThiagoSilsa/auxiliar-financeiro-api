ee1a404ef26b87183c8e7877d957ec26950d6a0f
ThiagoSilsa <thiagosantosprog@gmail.com>
2026-02-26 19:18:36 -0300

    feat(SequeizeConfig): Configure squlize e importei em server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,32 @@
 import app from "./app";
 import env from "./config/env";
+import sequelize from "./config/sequelize";
 
 const PORT = env.PORT;
+// Sincroniza o banco de dados
+sequelize
+  .then(async (db) => {
+    try {
+      // Sincroniza os modelos Sequelize com o banco de dados
+      await db.sync({ force: false });
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em ${PORT}`);
-});
+      // Inicia o servidor
+      app.listen(PORT, () => {
+        console.log(`🌐 Servidor rodando em http://localhost:${PORT}`);
+      });
+    } catch (error) {
+      // Caso ocorra algum erro durante a sincronização ou inicialização do servidor
+      console.error("Erro na inicialização do servidor:", error);
+      process.exit(1);
+    }
+  })
+  .catch((error) => {
+    // Caso ocorra falha na conexão inicial com o banco de dados
+    console.error("Erro de conexão com o banco de dados:", error);
+    process.exit(1);
+  });
+
+
+
+
+
