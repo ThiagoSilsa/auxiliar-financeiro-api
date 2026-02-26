a03b9290240abb128128c51a64f4035e144bfb6d
ThiagoSilsa <thiagosantosprog@gmail.com>
2026-02-26 20:03:03 -0300

    chore(app.ts): Corrigi erro de português

--- src/app.ts
+++ src/app.ts
@@ -11,5 +11,5 @@ indexRoute.forEach((route) => {
   app.use(`/v1${route.base}${route.path}`, route.handler);
 });
 
-console.log("Express incializado");
+console.log("Express inicializado");
 export default app;
