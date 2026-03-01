04aaf918314ac2cafb93fa50876736dbe6f46255
ThiagoSilsa <thiagosantosprog@gmail.com>
2026-03-01 07:39:44 -0300

    feat(UserRoutes): Reorganizei uso de rotas utilizando router do express

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,7 @@ app.use(express.json());
 app.use(morgan(":method :url :status :response-time ms"));
 
 // Rotas
-indexRoute.forEach((route) => {
-  app.use(`/v1${route.base}${route.path}`, route.handler);
-});
+app.use("/api/v1", indexRoute);
 
 console.log("Express inicializado");
 export default app;
